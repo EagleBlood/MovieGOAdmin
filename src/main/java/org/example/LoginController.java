80ee9544b4ab85de81ba679ac14cc9bac3050d45
Dawid Nosal <35221@student.pwsztar.edu.pl>
2023-05-23 14:13:02 +0200

    Proper login functionality, DB add movie functionality

--- src/main/java/org/example/LoginController.java
+++ src/main/java/org/example/LoginController.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -28,21 +31,23 @@ public class LoginController {
     }
 
     private void connectJDBC() throws IOException {
-//        String dbUrl = "jdbc:mysql://w10.domenomania.eu/wiktor10_kino";
-//        String username = inputUser.getText();
-//        String password = inputPass.getText();
-//
-//        try {
-//            Connection connection = DriverManager.getConnection(dbUrl, username, password);
-//            // Connection successful, perform database operations here
-//            System.out.println("Success");
-//            connection.close(); // Close the connection when you're done
-//        } catch (SQLException e) {
-//            System.out.println("GUNWOOOOOOOOOOOOOOOOOO!!11!!");
-//            e.printStackTrace();
-//        }
+        String dbUrl = LoginCredentials.getDbUrl();
+        String username = inputUser.getText();
+        String password = inputPass.getText();
+        LoginCredentials.setCredentials(username, password);
 
+        try {
+            Connection connection = DriverManager.getConnection(dbUrl, username, password);
+            // Connection successful, perform database operations here
+            System.out.println("Success");
+            App.setRoot("home");
+            connection.close(); // Close the connection when you're done
+        } catch (SQLException e) {
+            System.out.println("GUNWOOOOOOOOOOOOOOOOOO!!11!!");
+            e.printStackTrace();
+        }
 
-        App.setRoot("home");
     }
 }
+
+
