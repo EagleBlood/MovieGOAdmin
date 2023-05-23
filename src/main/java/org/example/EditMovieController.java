d22ef8c4b218fea5ec203859c1160745e2afff6a
Dawid Nosal <35221@student.pwsztar.edu.pl>
2023-05-23 17:37:38 +0200

    editMovie.fxml almost done, removeMovie.fxml started

--- src/main/java/org/example/EditMovieController.java
+++ src/main/java/org/example/EditMovieController.java
@@ -1,4 +1,98 @@
 package org.example;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditMovieController {
+
+    private String dbUrl = LoginCredentials.getDbUrl();
+    private String username = LoginCredentials.getUsername();
+    private String password = LoginCredentials.getPassword();
+
+    @FXML
+    private TableView<Movie> tableDB;
+    @FXML
+    private TableColumn<Movie, Integer> id_filmuCol;
+    @FXML
+    private TableColumn<Movie, String> tytulCol;
+    @FXML
+    private TableColumn<Movie, Integer> czas_trwaniaCol;
+    @FXML
+    private TableColumn<Movie, Double> ocenaCol;
+    @FXML
+    private TableColumn<Movie, String> opisCol;
+    @FXML
+    private TableColumn<Movie, Integer> id_gatunkuCol;
+    @FXML
+    private TableColumn<Movie, Byte[]> okladkaCol;
+    @FXML
+    private TableColumn<Movie, Double> cenaCol;
+    @FXML
+    private Button buttonLoadData;
+
+
+    //TODO
+    /* Edycja rekordów w bazie (funkcjonalność) */
+
+    public void initialize() {
+        id_filmuCol.setCellValueFactory(new PropertyValueFactory<>("id_filmu"));
+        tytulCol.setCellValueFactory(new PropertyValueFactory<>("tytul"));
+        czas_trwaniaCol.setCellValueFactory(new PropertyValueFactory<>("czas_trwania"));
+        ocenaCol.setCellValueFactory(new PropertyValueFactory<>("ocena"));
+        opisCol.setCellValueFactory(new PropertyValueFactory<>("opis"));
+        id_gatunkuCol.setCellValueFactory(new PropertyValueFactory<>("id_gatunku"));
+        okladkaCol.setCellValueFactory(new PropertyValueFactory<>("okladka"));
+        cenaCol.setCellValueFactory(new PropertyValueFactory<>("cena"));
+
+        buttonLoadData.setOnAction(event -> {
+            List<Movie> movieList = retrieveMoviesFromDatabase();
+            tableDB.setItems(FXCollections.observableArrayList(movieList));
+        });
+
+    }
+
+    private List<Movie> retrieveMoviesFromDatabase() {
+        List<Movie> movieList = new ArrayList<>();
+
+        try {
+            Connection connection = DriverManager.getConnection(dbUrl, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM film");
+            System.out.println("Połączono");
+
+            while (resultSet.next()) {
+                int id_filmu = resultSet.getInt("id_filmu");
+                String tytul = resultSet.getString("tytul");
+                int czas_trwania = resultSet.getInt("czas_trwania");
+                double ocena = resultSet.getDouble("ocena");
+                String opis = resultSet.getString("opis");
+                int id_gatunku = resultSet.getInt("id_gatunku");
+                byte[] okladka = resultSet.getString("okladka").getBytes();
+                double cena = resultSet.getDouble("cena");
+
+                Movie movie = new Movie(id_filmu, tytul, czas_trwania, ocena, opis, id_gatunku, okladka, cena);
+                movieList.add(movie);
+
+                System.out.println("Dodano rekord");
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle the database error gracefully
+        }
+
+        System.out.println("Koniec");
+        return movieList;
+    }
 }
---
+++ src/main/java/org/example/Movie.java
@@ -0,0 +1,87 @@
+package org.example;
+
+public class Movie {
+    private int id_filmu;
+    private String tytul;
+    private int czas_trwania;
+    private double ocena;
+    private String opis;
+    private int id_gatunku;
+    private byte[] okladka = null;
+    private double cena;
+
+    public Movie(int id_filmu, String tytul, int czas_trwania, double ocena, String opis, int id_gatunku, byte[] okladka, double cena) {
+        this.id_filmu = id_filmu;
+        this.tytul = tytul;
+        this.czas_trwania = czas_trwania;
+        this.ocena = ocena;
+        this.opis = opis;
+        this.id_gatunku = id_gatunku;
+        this.okladka = okladka;
+        this.cena = cena;
+    }
+    public int getId_filmu() {
+        return id_filmu;
+    }
+
+    public void setId_filmu(int id_filmu) {
+        this.id_filmu = id_filmu;
+    }
+
+    public String getTytul() {
+        return tytul;
+    }
+
+    public void setTytul(String tytul) {
+        this.tytul = tytul;
+    }
+
+    public int getCzas_trwania() {
+        return czas_trwania;
+    }
+
+    public void setCzas_trwania(int czas_trwania) {
+        this.czas_trwania = czas_trwania;
+    }
+
+    public double getOcena() {
+        return ocena;
+    }
+
+    public void setOcena(double ocena) {
+        this.ocena = ocena;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public int getId_gatunku() {
+        return id_gatunku;
+    }
+
+    public void setId_gatunku(int id_gatunku) {
+        this.id_gatunku = id_gatunku;
+    }
+
+    public byte[] getOkladka() {
+        return okladka;
+    }
+
+    public void setOkladka(byte[] okladka) {
+        this.okladka = okladka;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+
+}
