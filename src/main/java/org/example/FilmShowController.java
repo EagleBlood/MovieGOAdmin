f0efd068a549debd6e009bc9ed91354aaf7985ae
wiktor10 <wuszko1@gmail.com>
2023-05-24 17:32:26 +0200

    filmShow.fxml

--- src/main/java/org/example/FilmShowController.java
+++ src/main/java/org/example/FilmShowController.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class FilmShowController {
+    @FXML
+    private void backHome() throws IOException {
+        App.setRoot("home");
+    }
 }
