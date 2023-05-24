229c4f4c8c7568790c97479215963c7216f9e92c
wiktor10 <wuszko1@gmail.com>
2023-05-24 12:15:46 +0200

    changed styles

--- src/main/java/org/example/RemoveMovieController.java
+++ src/main/java/org/example/RemoveMovieController.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class RemoveMovieController {
+
+    @FXML
+    private void backHome() throws IOException {
+        App.setRoot("home");
+    }
 }
