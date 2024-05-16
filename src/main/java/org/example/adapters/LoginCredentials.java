e5d38ad06e6a7d3e36bf0708d71cb9c0569c9c19
Wiktor Uszko <77235682+wuszko10@users.noreply.github.com>
2024-05-16 17:59:48 +0200

    Update LoginCredentials.java

--- src/main/java/org/example/adapters/LoginCredentials.java
+++ src/main/java/org/example/adapters/LoginCredentials.java
@@ -3,7 +3,7 @@ package org.example.adapters;
 public class LoginCredentials {
     private static String username;
     private static String password;
-    private static String dbUrl = "jdbc:mysql://w10.domenomania.eu/wiktor10_kino";
+    private static String dbUrl = "jdbc:mysql:yourDomain";
 
     public static void setCredentials(String username, String password) {
         LoginCredentials.username = username;
